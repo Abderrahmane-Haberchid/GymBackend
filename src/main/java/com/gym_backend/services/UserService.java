77e3033a28a395747f7b25755f25d3d1fd6f8dfa
Abderrahmane-Haberchid <jhonsnowstark2@gmail.com>
2024-02-02 23:04:41 +0100

    first commit

--- src/main/java/com/gym_backend/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.gym_backend.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    UserDetailsService userDetailsService();
-}
